0fae74882c0b90490944a6e952dbc94dc7571052
jannuary <599700591@qq.com>
2019-05-03 19:35:53 +0800

    lexer初步完成

--- Compiler.js
+++ Compiler.js
@@ -1,6 +1,6 @@
 // 删除注释
-const rmComment = (datas) => {
-    const data = datas.toString().split("");
+const rmComment = (input) => {
+    const data = input.toString().split("");
 
     // 返回的数据
     let ret = { 
@@ -50,21 +50,53 @@ const rmComment = (datas) => {
 }
 
 // 词法分析器
+// 基本保留字1、标识符2、常数(字符串)3、运算符4、界符5
 const lexer = stream => {
     let data = stream.toString().replace(/[\r\n]/g, "");    // 去换行回车
     let chars = data.split("");         // 切割成字符
 
-    let str = "";
+    // 保留字字典1
+    let key = ["auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","redister","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while","include"];      
+    // 运算符字典4
+    let operator = ["+","-","*","/","%","="];  
+    // 界符字典5
+    let doundary = ["{", "}", ";", "\"", "\'", ",", "(", ")", "<", ">","[","]"];
+
+    // 映射
+    let vk = {
+        key: 1,
+        identifier:  2,
+        constant: 3,
+        operator: 4,
+        doundary: 5,
+    }
+    
+    // 返回
+    let output = [];
+
+    // 处理
+    // 数组化
     let arr = [];
     for (let i = 0; i < chars.length; i++) {
-        let [c, cn] = [chars[i], chars[i+1]];
+        let [cf, c, cn] = [chars[i-1], chars[i], chars[i+1]];
         
-        // 基本保留字、标识符、常数、运算符、界符
-        // 标示符，基本保留字
+        // 基本保留字，标示符，常数
         let patt = /[0-9a-zA-Z]/;
         if (patt.test(c)){
             let tmp = "";
-            while (patt.test(c)) {
+
+            if(cf=="\""){
+                while(!(c=="\"" && chars[i-1]!="\\")){
+                    tmp += c;
+                    c = chars[++i];
+                }
+            }else if(cf=="'"){
+                while (c != "'") {
+                    tmp += c;
+                    c = chars[++i];
+                }
+            }
+            else while (patt.test(c)) {
                 tmp += c;
                 c = chars[++i];
             }
@@ -73,25 +105,39 @@ const lexer = stream => {
             continue;
         }
 
-        // 常数
-
-
         // 运算符，界定符
         if(c==" "){
-            while (c == " ") {
+            while (c == " ") 
                 c = chars[++i];
-            }
             i--;
+            continue;
         }
         
-        
-        str += chars[i];
-        
-        
+        arr.push(chars[i])
 
     }
-    console.log(arr);
-    console.log(str);
+    // console.log(arr)
+    // 分类
+    // 保存形式 { type:' ', value:' '}
+    for (let i = 0; i < arr.length; i++) {
+        let type = undefined;
+        const [cf, c] = [arr[i-1], arr[i]];
+        if (key.find(x => c == x)) type = vk.key;   // 关键字
+        else if (operator.find(x => c == x)) type = vk.operator;      // 运算符
+        else if (doundary.find(x => c == x)) type = vk.doundary;      // 边界符
+        else if (/["']/.test(cf)) type = vk.constant;                 // 字符串
+        else if (/^[0-9]+.?[0-9]*$/.test(c)) type = vk.constant;
+        else type = vk.identifier;
+
+        output.push({
+            type: type,
+            value: c
+        })
+    }
+    console.log(output)
+
+    return output;
+
 }
 
 
